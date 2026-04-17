ba5d470b79cfacf61c9bab069e51134b8741b18f
Felipe Pereira Rodrigues de Souza <felipe.pereirasouza7@gmail.com>
2026-04-17 10:09:11 -0300

    Add files via upload

---
+++ Juros.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.juros;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class Juros {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    double juros, valor, rendimento;
+    
+    juros=0.70;
+    
+    
+    System.out.print("Digite o Valor Investido na Poupanca: ");
+    valor=sc.nextDouble();
+    
+    rendimento=valor*(1+juros*1);
+    System.out.println("Valor de Rendimento em 1 mes: " + rendimento);
+    }
+}
