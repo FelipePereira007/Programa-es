91f51b508bc91f543b1d9385cca6b0cfdaf31c37
Felipe Pereira Rodrigues de Souza <felipe.pereirasouza7@gmail.com>
2026-04-17 10:05:09 -0300

    Add files via upload

---
+++ ConsumoMedioDeCombustivelEmKmL.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.consumomediodecombustivelemkml;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class ConsumoMedioDeCombustivelEmKmL {
+
+   public static void main(String[] args) { {
+    
+        Scanner sc = new Scanner(System.in);
+                
+        int distancia, combustivelGasto, consumoMedio;
+        
+        
+        
+        
+        System.out.print("Digite a Distancia Percorrida em Km: ");
+        distancia=sc.nextInt();
+        
+        System.out.print("Digite a Quantia de Combustivel Gasto em L: ");
+        combustivelGasto=sc.nextInt();
+     
+      
+        
+        consumoMedio=distancia/combustivelGasto;
+        System.out.println("Consumo Medio em Km por L: " + consumoMedio);
+
+    }    }
+}
